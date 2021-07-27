bf1afa2d068394a6b12c9c480fe1f0206321aef3
GianTutorials <88041565+GianTutorials@users.noreply.github.com>
2021-07-27 15:44:27 +0200

    Add files via upload

---
+++ LoadindScreen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadindScreen : MonoBehaviour
+{
+    [SerializeField]
+    private Image progressBar;
+
+    [SerializeField]
+    private Text loadText;
+
+    [SerializeField]
+    private Button button;
+
+    void Start()
+    {
+        StartCoroutine(LoadingScreenAsyncOperation());
+    }
+
+    IEnumerator LoadingScreenAsyncOperation()
+    {
+        yield return null;
+        AsyncOperation loadScene = SceneManager.LoadSceneAsync(1);
+
+        loadScene.allowSceneActivation = false;
+
+        while (!loadScene.isDone)
+        {
+            progressBar.fillAmount = loadScene.progress;
+            loadText.text = (loadScene.progress * 100).ToString("F0") + "%";
+
+            if (loadScene.progress >= 0.9f)
+            {
+                progressBar.fillAmount = 1f;
+                loadText.text = "100%";
+            }
+
+            yield return null;
+        }
+    }
+}
